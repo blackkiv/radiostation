639c0f037214f9773c2146872a3ee932c981610d
livio-agolini-axon <lagolini@axon.dev>
2023-06-23 15:21:07 +0300

    init

---
+++ src/main/java/blck/radiostation/RadiostationApplication.java
@@ -0,0 +1,13 @@
+package blck.radiostation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiostationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiostationApplication.class, args);
+    }
+
+}
